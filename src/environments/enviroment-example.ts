44d4a5e4d51e7ec43b3752d1a7e000a4ffa96036
Weda Wesnawa <komangweda360@gmail.com>
2026-09-01 12:39:25 +0800

    forget delete

--- src/environments/enviroment-example.ts
+++ src/environments/enviroment-example.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiKey: 'AIzaSyCHsgrzPfOP62tGm7DLLNg-AHvfjEdYJT0', //upload dataset in gdrive
+  apiKey: 'YourAccesskey', //upload dataset in gdrive
   folderId: 'YourFolderIDHere'
 };
